5c83e143eaba91a1780ac67709e7d27c312b839f
Nandini Mundhra <nandinimundhra7@gmail.com>
2026-08-18 20:32:54 +0530

    setup the app and implement homepage and new book ui

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import {IBM_Plex_Serif, Mona_Sans } from "next/font/google";
+
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+const ibmPlexSerif=IBM_Plex_Serif({
+  variable:"--font-ibm-plex-serif",
+  subsets:['latin'],
+  weight: ['400','500','600','700'],
+  display:'swap'
+});
+
+const monaSans=Mona_Sans({
+  variable:"--font-mona-sans",
+  subsets:['latin'],
+  display:'swap'
+});
+
+export const metadata: Metadata = {
+  title: "AI Book Assistant",
+  description: "Transform your books into interactive AI conversations. Upload PDFs, and chat with your books using voice",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <ClerkProvider>
+    <html
+      lang="en"
+      className={`${ibmPlexSerif.variable} ${monaSans.variable} relative font-sans antialiased`}
+    >
+      
+      <body className="min-h-full flex flex-col">
+        <Navbar/>
+        {children}</body>
+    </html>
+    </ClerkProvider>
+  );
+}
